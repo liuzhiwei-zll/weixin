633fe3581feeba5ff9a4d322b2e45e23f3cc55ec
manager <manager@gmail.com>
2015-08-30 10:37:35 +0800

    添加了Person类

---
+++ weixin/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  weixin
+//
+//  Created by 刘志威 on 15/8/30.
+//  Copyright (c) 2015年 刘志威. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ weixin/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weixin
+//
+//  Created by 刘志威 on 15/8/30.
+//  Copyright (c) 2015年 刘志威. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
